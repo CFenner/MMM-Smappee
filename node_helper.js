f0d1b7192f2af9d754fba673df66d8ce643acac7
Christopher <Christopher.Fenner@me.com>
2018-01-23 23:04:32 +0100

    add node helper

---
+++ node_helper.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/* Magic Mirror
+ * Module: MMM-Smappee
+ *
+ * By Christopher Fenner http://github.com/CFenner
+ * MIT Licensed.
+ */
+
+const NodeHelper = require('node_helper');
+const oauth2 = require('simple-oauth2');
+
+module.exports = NodeHelper.create({
+    start: function () {
+        this.started = false;
+        console.log("Starting node helper for " + this.name);
+    },
+    getLocations: function(){},
+    getConsumption: function(){},
+    socketNotificationReceived: function(notification, payload) {
+        switch(notification){
+            case 'CONFIG':
+                if(this.started == false){
+                    const self = this;
+                    this.config = payload;
+                    this.started = true;
+                }
+                break;
+            case 'SMAPPEE_LOAD':
+                var self = this;
+
+                self.sendSocketNotification('SMAPPEE_DATA', {"consumption": 123});//JSON.parse(body));
+                break;
+            default:
+        }
+    }
+});
