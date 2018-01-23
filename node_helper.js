31b668c02f34c685a0c55d5a9ac24d95670f9a05
Christopher <Christopher.Fenner@me.com>
2018-01-24 00:04:08 +0100

    use token

--- node_helper.js
+++ node_helper.js
@@ -9,6 +9,7 @@
 
 const NodeHelper = require('node_helper');
 const oauth2 = require('simple-oauth2');
+const request = require('request');
 
 module.exports = NodeHelper.create({
     start: function () {
@@ -17,6 +18,9 @@ module.exports = NodeHelper.create({
     },
     getLocations: function(){},
     getConsumption: function(){},
+    getToken: function(){
+
+    },
     socketNotificationReceived: function(notification, payload) {
         console.log(notification, payload);
         switch(notification){
@@ -29,6 +33,34 @@ module.exports = NodeHelper.create({
                 break;
             case 'SMAPPEE_LOAD':
                 var self = this;
+
+                var auth = oauth2.create({
+                    client: {
+                        id: payload.client.id,
+                        secret: payload.client.secret
+                      },
+                      auth: {
+                        tokenHost: 'https://app1pub.smappee.net/dev/v1',
+                        tokenPath: '/oauth2/token'
+                      }
+                });
+
+                auth.ownerPassword.getToken({
+                    username: payload.user.id,
+                    password: payload.user.password
+                }).then((result) => {
+                    const accessToken = oauth2.accessToken.create(result);
+                    
+                    request.get(
+                        'https://app1pub.smappee.net/dev/v1/servicelocation/', 
+                        {'auth': {'bearer': accessToken.token}}
+                    ).on('response', function(response, body) {
+                        console.log(response.statusCode)
+                        console.log(response.headers['content-type'])
+                        console.log(body);
+                    })
+                    return accessToken;
+                });
 
                 self.sendSocketNotification('SMAPPEE_DATA', {"consumption": 123});//JSON.parse(body));
                 break;
