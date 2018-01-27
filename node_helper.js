8239ccd5750957fc16f0a8e42d0a35e47a3daaf3
Christopher <Christopher.Fenner@me.com>
2018-01-28 00:32:47 +0100

    remove logging commands
    #6

--- node_helper.js
+++ node_helper.js
@@ -18,72 +18,53 @@ const CONSUMPTION_PATH = '/{serviceId}/consumption';
 
 module.exports = NodeHelper.create({
     start: function () {
-        this.started = false;
         console.log("Starting node helper for " + this.name);
     },
     getLocations: function(){},
     getConsumption: function(){},
     getToken: function(){},
     socketNotificationReceived: function(notification, payload) {
-        console.log(notification, payload);
-        switch(notification){
-            case 'CONFIG':
-                if(this.started == false){
-                    const self = this;
-                    this.config = payload;
-                    this.started = true;
-                }
-                break;
-            case 'SMAPPEE_LOAD':
-                var self = this;
+        if('SMAPPEE_LOAD' === notification){
+            var self = this;
+            if(!self.auth){
+                self.auth = oauth2.create({
+                    client: {
+                        id: payload.client.id,
+                        secret: payload.client.secret
+                    },
+                    auth: {
+                        tokenHost: HOST,
+                        tokenPath: ENDPOINT + AUTH_PATH
+                    }
+                });
+            }
 
-                if(!self.auth){
-                    self.auth = oauth2.create({
-                        client: {
-                            id: payload.client.id,
-                            secret: payload.client.secret
-                        },
-                        auth: {
-                            tokenHost: HOST,
-                            tokenPath: ENDPOINT + AUTH_PATH
-                        }
-                    });
-                    console.log('oauth2 initialized');
-                }
-
-                self.auth.ownerPassword.getToken({
-                    username: payload.user.id,
-                    password: payload.user.password
-                }).then((result) => {
-                    self.accessToken = self.auth.accessToken.create(result);
-                    console.log('token created', self.accessToken);
-                    return request.get(
-                        HOST + ENDPOINT + SERVICE_PATH, {
-                        'auth': {'bearer': self.accessToken.token.access_token},
-                        json: true
-                    });
-                }).then((response) => {
-                    console.log('Location: ' + JSON.stringify(response));
-                    var location = response.serviceLocations[0].serviceLocationId;
-                    var to = new Date().getTime();
-                    return request.get(
-                        HOST + ENDPOINT + SERVICE_PATH + '/' + location + '/consumption', {
-                        'auth': {'bearer': self.accessToken.token.access_token},
-                        json: true,
-                        qs: {
-                            aggregation: 1,
-                            from: to - 900000,
-                            to: to,
-                        }
-                    });
-                }).then((response) => {
-                    console.log('Consumption: ' + JSON.stringify(response));
-                    var consumption = response.consumptions.pop()
-                    console.log('Response: ' + JSON.stringify(consumption));
-                    self.sendSocketNotification('SMAPPEE_DATA', consumption);
-                }).catch(console.log);
-                break;
-            default:
+            self.auth.ownerPassword.getToken({
+                username: payload.user.id,
+                password: payload.user.password
+            }).then((result) => {
+                self.accessToken = self.auth.accessToken.create(result);
+                return request.get(
+                    HOST + ENDPOINT + SERVICE_PATH, {
+                    'auth': {'bearer': self.accessToken.token.access_token},
+                    json: true
+                });
+            }).then((response) => {
+                var location = response.serviceLocations[0].serviceLocationId;
+                var to = new Date().getTime();
+                return request.get(
+                    HOST + ENDPOINT + SERVICE_PATH + '/' + location + '/consumption', {
+                    'auth': {'bearer': self.accessToken.token.access_token},
+                    json: true,
+                    qs: {
+                        aggregation: 1,
+                        from: to - 900000,
+                        to: to,
+                    }
+                });
+            }).then((response) => {
+                self.sendSocketNotification('SMAPPEE_DATA', response.consumptions.pop());
+            }).catch(console.log);
         }
     }
 });
